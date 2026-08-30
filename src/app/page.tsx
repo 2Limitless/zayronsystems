f3e1b8bf8a8da2904e3f071cb2e07c8cdb3505ba
Aaron <aarone0110@gmail.com>
2026-08-30 13:12:29 -0400

    feat: Restore 63509a1 exact mask math and resize inner button for perfect glass cushion with liquid transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -444,8 +444,8 @@ export default function Home() {
                         radial-gradient(circle, black 30px, transparent 30.5px),
                         linear-gradient(black, black),
                         linear-gradient(black, black),
-                        radial-gradient(circle, black 28px, transparent 28.5px),
-                        radial-gradient(circle, black 28px, transparent 28.5px),
+                        radial-gradient(circle, black 26px, transparent 26.5px),
+                        radial-gradient(circle, black 26px, transparent 26.5px),
                         radial-gradient(circle at 0% 100%, transparent 24px, black 24.5px),
                         radial-gradient(circle at 100% 100%, transparent 24px, black 24.5px);
                       mask-image: 
@@ -454,50 +454,50 @@ export default function Home() {
                         radial-gradient(circle, black 30px, transparent 30.5px),
                         linear-gradient(black, black),
                         linear-gradient(black, black),
-                        radial-gradient(circle, black 28px, transparent 28.5px),
-                        radial-gradient(circle, black 28px, transparent 28.5px),
+                        radial-gradient(circle, black 26px, transparent 26.5px),
+                        radial-gradient(circle, black 26px, transparent 26.5px),
                         radial-gradient(circle at 0% 100%, transparent 24px, black 24.5px),
                         radial-gradient(circle at 100% 100%, transparent 24px, black 24.5px);
                       
                       -webkit-mask-position: 
-                        left 30px top 0px,
-                        left 0px top 0px,
-                        right 0px top 0px,
-                        left calc(50% - 90px) top 60px,
-                        left calc(50% - 62px) bottom 0px,
-                        left calc(50% - 90px) bottom 0px,
-                        left calc(50% + 34px) bottom 0px,
-                        left calc(50% - 114px) top 60px,
-                        left calc(50% + 90px) top 60px;
+                        top center,
+                        top left,
+                        top right,
+                        center 60px,
+                        bottom center,
+                        calc(50% - 59px) bottom,
+                        calc(50% + 59px) bottom,
+                        calc(50% - 97px) 60px,
+                        calc(50% + 97px) 60px;
                       mask-position: 
-                        left 30px top 0px,
-                        left 0px top 0px,
-                        right 0px top 0px,
-                        left calc(50% - 90px) top 60px,
-                        left calc(50% - 62px) bottom 0px,
-                        left calc(50% - 90px) bottom 0px,
-                        left calc(50% + 34px) bottom 0px,
-                        left calc(50% - 114px) top 60px,
-                        left calc(50% + 90px) top 60px;
+                        top center,
+                        top left,
+                        top right,
+                        center 60px,
+                        bottom center,
+                        calc(50% - 59px) bottom,
+                        calc(50% + 59px) bottom,
+                        calc(50% - 97px) 60px,
+                        calc(50% + 97px) 60px;
                         
                       -webkit-mask-size: 
                         calc(100% - 60px) 60px,
                         60px 60px,
                         60px 60px,
-                        180px 28px,
-                        124px 56px,
-                        56px 56px,
-                        56px 56px,
+                        170px 26px,
+                        66px 52px,
+                        52px 52px,
+                        52px 52px,
                         24px 24px,
                         24px 24px;
                       mask-size: 
                         calc(100% - 60px) 60px,
                         60px 60px,
                         60px 60px,
-                        180px 28px,
-                        124px 56px,
-                        56px 56px,
-                        56px 56px,
+                        170px 26px,
+                        66px 52px,
+                        52px 52px,
+                        52px 52px,
                         24px 24px,
                         24px 24px;
                         
@@ -569,8 +569,8 @@ export default function Home() {
                   </div>
                 </div>
 
-                {/* Bottom Row (56px exactly, 180px wide) */}
-                <div className="md:hidden flex items-center justify-center h-[56px] w-[180px] relative z-10">
+                {/* Bottom Row (52px exactly, 170px wide) */}
+                <div className="md:hidden flex items-center justify-center h-[52px] w-[170px] relative z-10">
                   <button
                     onClick={() => handleNavClick("about")}
                     className={`relative w-[150px] h-[36px] flex items-center justify-center rounded-full text-center transition-colors duration-500 ${
